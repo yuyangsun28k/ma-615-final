7b7b77a6919811aba53a13a83776282bcc4bb57e
yuyangsun28k <tms28k@bu.edu>
2023-12-17 00:00:41 -0500

    add: personal settings of ui

---
+++ css.R
@@ -0,0 +1,27 @@
+###### personalized settings
+customCSS <- "
+/* Custom Header Style */
+.skin-blue .main-header .navbar {
+  background-color: #00ABC9; /* Aquamarine, like the Bahamas flag */
+}
+
+/* Custom Sidebar Style */
+.skin-blue .main-sidebar {
+  background-color: #FAE042; /* Gold, like the Bahamas flag */
+  color: black;
+}
+
+/* Custom color for sidebar menu items */
+.skin-blue .sidebar a {
+  color: black;
+}
+
+/* Custom Sidebar Style */
+.skin-blue .main-sidebar {
+  height: 100vh; /* Full height of the viewport */
+  position: fixed; /* Fixed position */
+  overflow-y: auto; /* Scrollbar for overflow content */
+  top: 0;
+  left: 0;
+}
+"
